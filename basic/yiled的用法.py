80550968f74fe5cc3e82894ab2ed3252feeb122b
zhw <1914624121@qq.com>
2019-12-27 23:34:25 +0800

    Initial commit

---
+++ basic/yiled的用法.py
@@ -0,0 +1,28 @@
+# def h():
+#     print ('Wen Chuan'),
+#     m = yield 5  # Fighting!
+#     print (m)
+#     d = yield 12
+#     print  ('We are together!')
+# c = h()
+# next(c)  #相当于c.send(None)
+# print(type(c))
+# c.send('Fighting!')  #(yield 5)表达式被赋予了'Fighting!'
+
+
+def h():
+    print ('Wen Chuan'),
+    m = yield 5  # Fighting!
+    print (m)
+    d = yield 12
+    print(d)
+    print ('We are together!')
+    yield 9
+c = h()
+m = next(c)  #m 获取了yield 5 的参数值 5
+
+d = c.send('Fighting!')  #d 获取了yield 12 的参数值12
+c.send("zhw")
+print ('We will never forget the date', m, '.', d)
+
+
